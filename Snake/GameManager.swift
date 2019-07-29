33f3861f4e96d7a840344c912e43f7fa39c1f209
MattHew Phraxayavong <mphraxa@gmail.com>
2019-07-29 07:15:25 -0700

    added game menu and push to start creates background board

---
+++ Snake/GameManager.swift
@@ -0,0 +1,26 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by MattHew Phraxayavong on 7/23/19.
+//  Copyright © 2019 Matthew Phraxayavong. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    vart scene: GameScene!
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    
+}
+
+
+//class GameManager {
+//
+//    var scene: GameScene!
+//    init(scene: GameScene) {
+//        self.scene = scene
+//    }
+//}
